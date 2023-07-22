3e3516128d6ddf0d9b1d994433a29030048b7b67
Adam Chalkley <atc0005@users.noreply.github.com>
2023-07-22 12:02:04 -0500

    Add initial support for decoding text streams
    Add prototype `dsl` tool for decoding Safe Links URLs within
    surrounding input text.
    
    README coverage updated to cover new tool. Special emphasis
    added on potential future support for the `dsl` tool for
    features not currently available. Originally the idea was to
    provide feature parity between the `dsl` and `usl` tools but
    I want to consider that further before implementation.
    
    refs GH-185

---
+++ cmd/dsl/main.go
@@ -0,0 +1,103 @@
+// Copyright 2022 Adam Chalkley
+//
+// https://github.com/atc0005/safelinks
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
+//go:generate go-winres make --product-version=git-tag --file-version=git-tag
+
+package main
+
+/*
+	Misc resources reviewed/consulted while building this:
+
+	https://www.reddit.com/r/golang/comments/fsxkqr/cancelling_blocking_read_from_stdin/
+	https://play.golang.com/p/Ff8Grkobd7L
+	https://github.com/golang/go/issues/24842#issuecomment-382384215
+	https://superuser.com/questions/296969/re-enter-interactive-mode-after-ctrl-z
+	https://stackoverflow.com/questions/34481065/break-out-of-input-scan
+	https://stackoverflow.com/questions/43947363/detect-if-a-command-is-piped-or-not
+	https://stackoverflow.com/questions/22744443/check-if-there-is-something-to-read-on-stdin-in-golang
+	https://stackoverflow.com/a/26567513/903870
+	https://www.honeybadger.io/blog/a-definitive-guide-to-regular-expressions-in-go/
+
+	https://pkg.go.dev/os/signal#NotifyContext
+*/
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	userFeedbackOut := os.Stderr
+	// debugLoggingOut := os.Stderr // switch to os.Stderr for debugging
+	debugLoggingOut := io.Discard // use io.Discard for normal operation
+	log.SetOutput(debugLoggingOut)
+
+	// We wrap the os.Exit() behavior so that we can safely use deferred
+	// cleanup functionality.
+	var appExitCode int
+	defer func(code *int) {
+		var exitCode int
+		if code != nil {
+			exitCode = *code
+		}
+		os.Exit(exitCode)
+	}(&appExitCode)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	timerDuration := getAppTimeout()
+
+	// TODO: Consider using signal.NotifyContext in place of current shutdown
+	// handling logic as this could simplify the implementation.
+	//
+	// https://pkg.go.dev/os/signal#NotifyContext
+	signalChan, done, timer := setupShutdownHandling(timerDuration)
+
+	defer func() {
+		log.Println("Cleaning up signal catch behavior")
+		signal.Stop(signalChan)
+
+		log.Println("Cancelling base context")
+		cancel()
+	}()
+
+	go shutdownListener(cancel, userFeedbackOut, timer, signalChan, done)
+
+	resultsChan := make(chan string)
+	errChan := make(chan error, 1)
+
+	go pollInputSource(ctx, os.Stdin, timer, timerDuration, resultsChan, errChan, done)
+
+	showAppUsageInfo(os.Stdin, timerDuration, userFeedbackOut)
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Context expired.")
+
+			return
+
+		case result := <-resultsChan:
+			fmt.Fprintln(os.Stdout, result)
+
+		case err := <-errChan:
+			fmt.Fprintln(userFeedbackOut, err.Error())
+			appExitCode = 1
+
+			return
+
+		case <-done:
+			log.Println("done channel activated. Exiting.")
+
+			return
+		}
+	}
+}
